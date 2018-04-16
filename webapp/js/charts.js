d3d4adc5ca90d32edbc1ab5e0d4026eac6399d45
Rafael Rodriguez <rafael.roguz@gmail.com>
2018-04-16 17:09:41 -0500

    gráfica de líneas

--- webapp/js/charts.js
+++ webapp/js/charts.js
@@ -1,39 +1,58 @@
 window.onload = function () {
-    var ctx = $("#myChart");
-
-    var myChart = new Chart(ctx, {
-        type: 'bar',
+    //Gráfica de línea para el total de ventas mensusales
+    new Chart(document.getElementById("graficaLinea"), {
+        type: 'line',
         data: {
-            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
             datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
+                data: [6412, 6541, 7425, 7457],
+                label: "2018",
+                borderColor: "#3e95cd",
+                fill: false,
+                pointRadius: 8,
+                pointHoverRadius: 12
+            }, {
+                data: [6525, 7220, 5412, 6022, 6354, 8019, 6245, 5702, 6700, 5567, 6421, 7952],
+                label: "2017",
+                borderColor: "#8e5ea2",
+                fill: false,
+                pointRadius: 8,
+                pointHoverRadius: 12
+            }, {
+                data: [5512, 5621, 5876, 6548, 6584, 5658, 6845, 6896, 6895, 7343, 7458, 7542],
+                label: "2016",
+                borderColor: "#3cba9f",
+                fill: false,
+                pointRadius: 8,
+                pointHoverRadius: 12
             }]
         },
         options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero:true
-                    }
-                }]
+            title: {
+                display: true,
+                text: 'Total de ventas por mes'
+            }
+        }
+    });
+
+    //Gráfica de barras para el top 5 de productos más vendidos
+    new Chart(document.getElementById("graficaBarra"), {
+        type: 'bar',
+        data: {
+            labels: ["PlayStation 4", "Nintendo Switch", "Macbook Pro", "Bocina JBL GO", "iPhone X"],
+            datasets: [
+                {
+                    label: "Unidades vendidas",
+                    backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+                    data: [5478,5267, 3785, 2564, 1668]
+                }
+            ]
+        },
+        options: {
+            legend: { display: false },
+            title: {
+                display: true,
+                text: 'Top productos más vendidos (semanal)'
             }
         }
     });
